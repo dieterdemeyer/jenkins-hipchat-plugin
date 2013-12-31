0e5bcb76e17cbee6ee0dcc785aa5e41064f5fe1c
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-12-31 19:00:40 +0100

    Changes to hipchat plugin the way I like it.. I hope this actually works because I'm confused...

--- src/main/java/jenkins/plugins/hipchat/HipChatService.java
+++ src/main/java/jenkins/plugins/hipchat/HipChatService.java
@@ -1,7 +1,6 @@
 package jenkins.plugins.hipchat;
 
 public interface HipChatService {
-    void publish(String message);
-
-    void publish(String message, String color);
+   void publish(String message);
+   void publish(String message, String color);
 }
